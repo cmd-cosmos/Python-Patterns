8a13e88f6201a84ac56ba9847d4eaea288546519
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-03 02:07:37 -0500

    rocket factory class implementation to ensure no ownership in the main function and cleaner flow

--- 1-creational/factory.py
+++ 1-creational/factory.py
@@ -3,36 +3,80 @@
 
 # factory: separate creation from use
 
-class Falcon:
+if 0:
+    class Falcon:
+        def manufacture(self):
+            print("Manufacturing a Falcon-9.")
+
+    class FalconHeavy:
+        def manufacture(self):
+            print("Manfacturing a Falcon Heavy.")
+
+    class Starship:
+        def manufacture(self):
+            print("Manfacturing a Starship Superheavy.")
+
+    def main():
+        inp = input("what kind of rocket should we manufature: ")
+
+        if inp == "f9":
+            f9 = Falcon()
+            f9.manufacture()
+            return
+        elif inp =="fh":
+            fh =  FalconHeavy()
+            fh.manufacture()
+            return
+        elif inp == "ss":
+            ss = Starship()
+            ss.manufacture()
+            return
+        else:
+            print("Invalid Vehicle ID")
+            return
+#####################################################################################
+    
+from abc import ABC, abstractmethod
+
+class RocketABC(ABC):
+    @abstractmethod
+    def manufacture(self):
+        pass
+
+class Falcon9(RocketABC):
     def manufacture(self):
         print("Manufacturing a Falcon-9.")
-
-class FalconHeavy:
+        
+class FalconHeavy(RocketABC):
     def manufacture(self):
         print("Manfacturing a Falcon Heavy.")
     
-class Starship:
+class Starship(RocketABC):
     def manufacture(self):
         print("Manfacturing a Starship Superheavy.")
+
+class RocketFactory:
+    id_map = {
+        "f9" : Falcon9,
+        "fh" : FalconHeavy,
+        "ss" : Starship,
+    }
+
+    @staticmethod
+    def manufacture_rocket(id_inp):
+        selected_class = RocketFactory.id_map.get(id_inp.lower())
+        if selected_class is None:
+            raise ValueError("Invalid Vehicle ID")
+        return selected_class()
     
 def main():
-    inp = input("what kind of rocket should we manufature: ")
+    inp = input("enter LV id (f9/fh/ss): ")
 
-    if inp == "f9":
-        f9 = Falcon()
-        f9.manufacture()
-        return
-    elif inp =="fh":
-        fh =  FalconHeavy()
-        fh.manufacture()
-        return
-    elif inp == "ss":
-        ss = Starship()
-        ss.manufacture()
-        return
-    else:
-        print("Invalid Vehicle ID")
-        return
+    try:
+        obj = RocketFactory.manufacture_rocket(inp)
+        obj.manufacture()
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
