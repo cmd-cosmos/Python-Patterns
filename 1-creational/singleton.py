28baf8d12172965965adc28f5c501ee042b4c2c4
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-03 01:20:41 -0500

    added a basic singleton class for the same tower class.

--- 1-creational/singleton.py
+++ 1-creational/singleton.py
@@ -3,10 +3,15 @@
 # type: ignore
 
 # ensure single instance of a class exists at a given time
+# __init__ --> controls initialization of an instance
+# __new__ ---> controls how new instance is created
+
+from typing_extensions import Self
+
 
 class RegularControlTower:
     def __init__(self) -> None:
-        print("Constructing Control Tower Object...")
+        print("Constructing Control Tower Object.")
 
 # 2 objects with unique ids --> 2 separate instances of the class
 t1 = RegularControlTower()
@@ -15,3 +20,26 @@ t2 = RegularControlTower()
 print("t1 id: ", id(t1))
 print("t2 id: ", id(t2))
 print(t1 is t2)  # False --> as both objs have different ids
+
+print("*"*70)
+
+class SingletonControlTower:
+    _instance = None # track any active instance to ensure not more than 1 created
+   
+    def __new__(cls) -> Self:
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            print("Constructing New Control Tower Object")
+        else:
+            print("Using previously created object for repeated instance req.")
+        return cls._instance
+        
+
+st1 = SingletonControlTower()
+st2 = SingletonControlTower()
+
+print("st1 id: ", id(st1))
+print("st2 id: ", id(st2))
+print(st1 is st2)  # True --> as both objs have same ids
+
+
